8ad41012c817a0fa991b78114bcba6afdec5a602
waryansingh <70606387+waryansingh@users.noreply.github.com>
2024-08-12 15:08:33 +0530

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1.0">
+        <title>Login Form in HTML and CSS | Codehal</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+     
+        <div class="input-box"> 
+            <form action="index.php" method="post">
+                <h1>Login</h1>
+                <input type="text" placeholder="Username" name="username" required>
+        </div>
+
+        <div class="input-box">
+            <input type="password" placeholder="Password" name="password" required>
+        </div>
+
+        <div class="remember-forgot">
+            <label><input type="checkbox"> Remember me</label>
+            <a href="#">Forgot password?</a>
+        </div>
+
+            <button type="submit" class="btn" name="login">login</button>
+        <div>
+            <p> for new account
+                <a href="#"> sign in </a></p>
+            </form>
+            </form>
+        </div>
+
+    </body>
+ </html>
+
+
+
+
+
+<!-- <?php
+if (isset($_POST["login"]))
+{
+    echo $_POST["username"];
+    echo $_POST["password"];
+}
+?> -->
